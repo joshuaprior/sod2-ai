f761f60d1f31ba1e34415cbb0c4ebda68747b198
Joshua Prior <joshuaprior.github@gmail.com>
2026-03-12 14:38:18 -0700

    Add more small facilities and prep for large facilities

--- src/ai/synthetic_data/menus/menus.py
+++ src/ai/synthetic_data/menus/menus.py
@@ -2,6 +2,14 @@ from enum import Enum
 
 class Menu(Enum):
     # Metadata: (Class Import Path, Icon File Path)
+    FIGHTING_GYM = (
+        "src.ai.synthetic_data.menus.classes.small_facility.SmallFacility",
+        "src/ai/synthetic_data/menus/icons/facility/small/fighting_gym.bmp"
+    )
+    SHOOTING_RANGE= (
+        "src.ai.synthetic_data.menus.classes.small_facility.SmallFacility",
+        "src/ai/synthetic_data/menus/icons/facility/small/shooting_range.bmp"
+    )
     WORKSHOP = (
         "src.ai.synthetic_data.menus.classes.small_facility.SmallFacility",
         "src/ai/synthetic_data/menus/icons/facility/small/workshop.bmp"
